b3d5b608895442eb45f344281f374662f7cd4296
Cristhian27A <alonsorodriguez2772gmail.com>
2024-10-16 14:45:45 -0500

    add docs

---
+++ app/iterador.py
@@ -0,0 +1,11 @@
+#se crea una lista
+my_list = [1,2,3,4]
+
+#se obtiene el iterador
+my_iter = iter(my_list)
+
+#Uso del iterador para almacenar en memoria
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
